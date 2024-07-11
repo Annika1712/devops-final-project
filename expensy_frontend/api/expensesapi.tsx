751f10573a0eb65b7ba4d838ab552fc51ebce19e
Olha Kharabet <kharabet.olga@gmail.com>
2024-07-11 22:41:11 +0200

    Created docker-compose.yml for local testing, Dockerfiles for images, fixed .env for frontend app

--- expensy_frontend/api/expensesapi.tsx
+++ expensy_frontend/api/expensesapi.tsx
@@ -9,13 +9,11 @@ import apiClient from "./apiclient"
 
 //   curl -X GET http://localhost:3000/api/expenses -H "Content-Type: application/json"
 
-
 const fetchExpensesAPI = async () => {
-   
-        const response = await apiClient.get("/api/expenses")
-       return response
-   
-    }
+  const response = await apiClient.get("/api/expenses")
+  return response
+
+}
 
 const addExpensesAPI = async (name: string, amount: number, category: string) => {
     const response = await apiClient.post("/api/expenses", {

79bb179ba616ed619dd7a98b8f9d084fd8f82a28
Annika1712 <annika-strooper17@hotmail.nl>
2024-07-26 18:46:52 +0200

    ci-cd

--- terraform/k8s/storageclass.tf
+++ terraform/k8s/storageclass.tf
@@ -6,7 +6,4 @@ resource "kubernetes_storage_class_v1" "ebs" {
   reclaim_policy         = "Retain"
   volume_binding_mode    = "WaitForFirstConsumer"
   allow_volume_expansion = true
-  parameters = {
-    type = "pg3"
-  }
 }
